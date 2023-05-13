fc062520ee049567e98021869fa9b643d2396cd4
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-12 22:28:30 -0300

    Carregar imagens na tela de controle de orçamento

--- src/components/ControlPresupuesto.js
+++ src/components/ControlPresupuesto.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
+import globalStyles from '../styles';
 
 const ControlPresupuesto = () => {
-  return <Text>ControlPresupuesto</Text>;
+  return (
+    <View style={styles.contenedor}>
+      <View style={styles.centrarGrafica}>
+        <Image source={require('../img/grafico.jpg')} style={styles.imagen} />
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    ...globalStyles.contenedor,
+  },
+  centrarGrafica: {
+    alignItems: 'center',
+  },
+  imagen: {
+    width: 250,
+    height: 250,
+  },
+});
 
 export default ControlPresupuesto;
--- src/components/NuevoPresupuesto.js
+++ src/components/NuevoPresupuesto.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {View, Text, TextInput, Pressable, StyleSheet} from 'react-native';
+import globalStyles from '../styles';
 
 const NuevoPresupuesto = ({handleNuevoPresupuesto}) => {
   const [presupuesto, setPresupuesto] = useState(0);
@@ -27,21 +28,7 @@ const NuevoPresupuesto = ({handleNuevoPresupuesto}) => {
 
 const styles = StyleSheet.create({
   contenedor: {
-    backgroundColor: '#fff',
-    marginHorizontal: 10,
-    borderRadius: 10,
-    paddingVertical: 40,
-    paddingHorizontal: 20,
-    transform: [{translateY: 50}],
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-
-    elevation: 4,
+    ...globalStyles.contenedor,
   },
   label: {
     textAlign: 'center',
