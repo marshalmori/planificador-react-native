19474f7ce2e5838534d7f24ed6991ab33c4f48bb
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-16 20:20:01 -0300

    Mostrar as imagens das categorias dos gastos

--- src/components/Gasto.js
+++ src/components/Gasto.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import {Text, View, StyleSheet, Image, Pressable} from 'react-native';
 import globalStyles from '../styles';
+import {formatearCantidad} from '../helpers';
+
+const diccionarioIconos = {
+  ahorro: require('../img/icono_ahorro.png'),
+  comida: require('../img/icono_comida.png'),
+  casa: require('../img/icono_casa.png'),
+  gastos: require('../img/icono_gastos.png'),
+  ocio: require('../img/icono_ocio.png'),
+  salud: require('../img/icono_salud.png'),
+  suscripciones: require('../img/icono_suscripciones.png'),
+};
 
 const Gasto = ({gasto}) => {
   const {nombre, categoria, cantidad, id} = gasto;
   return (
-    <View>
-      <Text>{nombre}</Text>
+    <View style={styles.contenedor}>
+      <View>
+        <View>
+          <Image source={diccionarioIconos[categoria]} />
+          <View>
+            <Text>{categoria}</Text>
+            <Text>{nombre}</Text>
+          </View>
+        </View>
+
+        <Text>{formatearCantidad(cantidad)}</Text>
+      </View>
     </View>
   );
 };
